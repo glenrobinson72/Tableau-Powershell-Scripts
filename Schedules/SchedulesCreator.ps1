023dac74d6fefce54ab4fb4ca1aaa52eeb4e50b0
Glen Robinson <glen.david.robinson@gmail.com>
2020-08-27 12:56:57 +0100

    Require Tableau-REST_3.8.psm1 (minimum to work)

---
+++ Schedules/SchedulesCreator.ps1
@@ -0,0 +1,28 @@
+﻿ param(
+ [string[]] $server,
+ [string[]] $username,
+ [string[]] $password,
+ [validateset('http','https')][string[]] $protocol = 'http',
+ [validateset('True','False')][string[]] $disable = 'False',
+ [string[]] $file = "C:\temp\schedules.csv"
+ )
+
+TS-SignIn -server $server -username $username -password $password -protocol $protocol
+
+$Schedules = Import-Csv -Delimiter ";" -Path $file
+
+ForEach ($schedule in $Schedules)
+ {
+    $schedule
+    if ($schedule.End -ne ''){$EndTime = ($schedule.End).Substring(0,5)}else{$EndTime = ''}
+
+    TS-CreateSchedule -ScheduleName $schedule.name -Priority $schedule.priority -Type $schedule.type -ExecutionOrder $schedule.executionOrder -State $schedule.state -Frequency $schedule.Frequency -StartTime ($schedule.Start).Substring(0,5) -EndTime $EndTime -Interval $schedule.Interval
+
+    if ($disable -eq 'True')
+        {
+         Start-Sleep -Seconds 1
+         TS-UpdateSchedule -ScheduleName $schedule.name  -newState Suspended
+        }
+ }
+
+TS-SignOut
---
+++ Schedules/SchedulesExporter.ps1
@@ -0,0 +1,58 @@
+﻿ param(
+ [string[]] $server,
+ [string[]] $username,
+ [string[]] $password,
+ [validateset('http','https')][string[]] $protocol = 'http',
+ [string[]] $file = "C:\temp\schedules.csv"
+ )
+
+TS-SignIn -server $server -username $username -password $password -protocol $protocol
+
+$schedules = TS-QuerySchedules
+"Name;State;Priority;Type;Frequency;ExecutionOrder;Start;End;Interval" > $file
+
+ForEach ($schedule in $schedules)
+  {
+    $scheduleDetails = TS-UpdateSchedule -ScheduleName $schedule.name
+    $scheduleDetails
+    $Freqlist = ""
+    switch ($scheduleDetails.Frequency)
+     {
+
+     "Weekly"
+      {
+       "Weekly"
+        ForEach ($Interval in $scheduleDetails.Weekdays)
+        { $Freqlist += $Interval + "," }
+        $scheduleDetails.Name + ";" + $scheduleDetails.State + ";" + $scheduleDetails.Priority + ";" + $scheduleDetails.Type + ";" + $scheduleDetails.Frequency + ";" + $scheduleDetails.ExecutionOrder + ";" + $scheduleDetails.frequencyStart+ ";" + $scheduleDetails.frequencyEnd + ";" + $FreqList >> $file        
+
+     }
+
+     "Monthly"
+      {
+       "Monthly"
+        $Freqlist = $scheduleDetails.DayofMonth
+        $scheduleDetails.Name + ";" + $scheduleDetails.State + ";" + $scheduleDetails.Priority + ";" + $scheduleDetails.Type + ";" + $scheduleDetails.Frequency + ";" + $scheduleDetails.ExecutionOrder + ";" + $scheduleDetails.frequencyStart+ ";" + $scheduleDetails.frequencyEnd + ";" + $FreqList >> $file        
+
+     }
+
+     "Daily"
+      {
+       "Daily"
+       $scheduleDetails.Name + ";" + $scheduleDetails.State + ";" + $scheduleDetails.Priority + ";" + $scheduleDetails.Type + ";" + $scheduleDetails.Frequency + ";" + $scheduleDetails.ExecutionOrder + ";" + $scheduleDetails.frequencyStart+ ";" + $scheduleDetails.frequencyEnd + ";" + $FreqList >> $file        
+
+     }
+
+     "Hourly"
+      {
+       "Hourly"
+       $scheduleDetails
+
+       $Freqlist = $scheduleDetails.Hours + $scheduleDetails.Minutes 
+       $scheduleDetails.Name + ";" + $scheduleDetails.State + ";" + $scheduleDetails.Priority + ";" + $scheduleDetails.Type + ";" + $scheduleDetails.Frequency + ";" + $scheduleDetails.ExecutionOrder + ";" + $scheduleDetails.frequencyStart+ ";" + $scheduleDetails.frequencyEnd + ";" + $FreqList >> $file        
+     }
+   }
+
+  }
+
+TS-SignOut
